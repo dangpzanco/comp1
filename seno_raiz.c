97a7fb36cb6a590568d90ad9862656c73e0b9953
dangpzanco <dangpzanco@gmail.com>
2013-09-30 17:52:38 -0300

    Update r2

--- seno_raiz.c
+++ seno_raiz.c
@@ -1,17 +1,17 @@
-/*****************************************************************/
+/*******************************************************************************************/
 /*  Computacao Cientifica - EEL 7021
 /*  Grupo 11A
 /*         Daniel Gomes de Pinho Zanco         Matricula: 13100574
 /*         Mateo Daniel Roig Greidanus         Matricula: 13100601
 /*         T3
 /*	Compilado e testado no Code::Blocks
-/*****************************************************************/
+/*  Pode ser conferido no site: https://github.com/dangpzanco/comp1/blob/master/seno_raiz.c
+/*******************************************************************************************/
 
-// Inclúi as bibliotecas necessárias e define o número pi
+// Inclúi as bibliotecas necessárias
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define PI 3.14159265358979323846; // Também poderia ser: const double pi = 4. * atan(1.);
 
 // Declaração das funções utilizadas
 int fator(int);
@@ -20,7 +20,7 @@ double seno(double, int);
 
 int main()
 {
-	printf("Calculadora do seno de um angulo em graus\n\n");
+	printf("Calculadora do seno de um angulo em graus (favor ler comentarios da funcao no codigo)\n\n");
 
 	// Declaração de variáveis: x = input, y = output do aluno, z = output do math.h e n = número de interações
 	double x, y, z, n;
@@ -33,15 +33,11 @@ int main()
 	printf("\nInsira o numero de interacoes que deseja para calcular o seno: ");
 
 	// Atribui o valor desejado ao número de interações
-	scanf("%i", &n);
+	scanf("%d", &n);
 
-	// Conversão de graus para radianos
-	x *= PI;
-	x /= 180.0;
-
-	// Atribuição de valores às variáveis de output
-	y = seno(x, n);
-	z = sin(x);
+	// Atribuição de valores às variáveis de output com conversão de graus para radianos
+	y = seno(x * 3.14159265358979323846 / 180, n);
+	z = sin(x * 3.14159265358979323846 / 180);
 
 	// Exibição dos resultados na tela
 	printf("\nFuncao do aluno: %lf\n", y);
@@ -73,12 +69,17 @@ int fator(int n)
 	else return 1;
 }
 
-// Utiliza uma série de Taylor para o cálculo do seno de x.
+// Utiliza uma série de Taylor para o cálculo do seno de x, com x em radianos.
+// Por algum motivo a função está retornando o arcosseno do seno do angulo "x",
+// por exemplo: com x = 30 graus, o seno de x é igual a 0.5 e arcosseno(0.5 radianos) = 0.523599,
+// a função desenvolvida retorna 0.523599 neste caso, apesar de utilizar a série de Taylor correta, conforme a fonte.
+// Essa função utiliza da recursividade como principal recurso, assim como a função fatorial,
+// formando uma série que vai até onde o usuário necessitar.
 // Fonte: https://pt.wikipedia.org/wiki/Seno
 double seno(double x, int n)
 {
-	if (n > 0) return (pow(-1, n)*pow(x, 2*n + 1))/fator(2*n + 1) + seno(x, n - 1);
-	else return x;
+	if (n >= 0) return (pow(-1.0, n)*pow(x, 2.0*n + 1.0))/fator(2.0*n + 1.0) + seno(x, n - 1.0);
+	else return 0.0;
 }
 
 // Implementação da função raiz quadrada utilizando um inline assembly code.
